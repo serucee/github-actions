0d054948f3524b806245cbc59e7f1ce94d95e4fe
serucee <sebastian.russmann@wirecard.com>
2019-10-29 09:19:20 +0100

    Test semantic release

---
+++ .releaserc.js
@@ -0,0 +1,55 @@
+function camelCaseToEnglish(string) {
+    return string.replace( /([A-Z])/g, " $1" );
+}
+
+function addNormalizedHeader(context, options) {
+    context.commitGroups = context.commitGroups.map(group => {
+        group.title = camelCaseToEnglish(group.title);
+
+        return group
+    });
+
+    return context
+}
+
+function resolveVersionNumbers() {
+    const versions = [
+        { label: "PHP", compatibility: "5.6, 7.0, 7.1, 7.2", tested: "7.2" },
+    ];
+
+    return versions
+}
+
+module.exports = {
+    "plugins": [
+        ["@semantic-release/commit-analyzer", {
+            "preset": "eslint",
+            "releaseRules": [
+                { "tag": "Breaking", "release": "major" },
+                { "tag": "PaymentMethods", "release": "minor" },
+                { "tag": "Feature", "release": "minor" },
+                { "tag": "Fix", "release": "patch" }
+            ]
+        }],
+
+        ["@semantic-release/release-notes-generator", {
+            "preset": "eslint",
+            "writerOpts": {
+                "commitPartial": "* {{ message }}\n",
+                "finalizeContext": addNormalizedHeader
+            }
+        }],
+
+        ["@semantic-release/github", {
+            "assets": [
+                { "path": "wirecardpaymentgateway.zip", "label": "Wirecard PrestaShop Module" }
+            ]
+        }],
+
+        ['@semantic-release/npm', { npmPublish: false }],
+
+        ["semantic-release-compatibility-table", {
+            resolveVersionNumbers
+        }]
+    ]
+};
